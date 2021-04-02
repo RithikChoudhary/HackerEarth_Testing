e3d107decb210c12f0633974b723b1235bde2e6e
Rithik Choudhary <40898779+burnt776@users.noreply.github.com>
2021-04-03 01:32:54 +0530

    Update data_provider.java

--- TrueCaller/data_provider.java
+++ TrueCaller/data_provider.java
@@ -8,11 +8,9 @@ public class data_provider {
 	{
 		Object[][] a=new Object[2][2];
 		
-		a[0][0]="burnt776@gmail.com";
-		a[0][1]="Bilupilu@1173";
+		a[0][0]="Email@gmail.com";
+		a[0][1]="Password";
 		
-//		a[1][0]="500069261@stu.upes.ac.in";
-//		a[1][1]="bilupilu@@";
 				
 		
 		return a;
