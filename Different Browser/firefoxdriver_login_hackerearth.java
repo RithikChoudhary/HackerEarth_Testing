7ed5a2374ac41a59b94ee047f62ecdd69f2ff097
SUMYAK JAIN <3456gaurav@gmail.com>
2021-04-07 10:51:45 +0530

    Update firefoxdriver_login_hackerearth.java

--- Different Browser/firefoxdriver_login_hackerearth.java
+++ Different Browser/firefoxdriver_login_hackerearth.java
@@ -1,4 +1,3 @@
-
 import org.openqa.selenium.By;
         import org.openqa.selenium.JavascriptExecutor;
         import org.openqa.selenium.WebDriver;
@@ -11,23 +10,20 @@ public class firefoxdriver_login_hackerearth {
 
     public static void main(String[] args) {
 
-        //edge driver
-       // System.setProperty("webdriver.edge.driver", "D:\\msedgedriver.exe");
-       // WebDriver driver = new EdgeDriver();
-
-        //firefox driver
+   
+        firefox driver
         System.setProperty("webdriver.gecko.driver", "D:\\geckodriver.exe");
-        WebDriver driver = new FirefoxDriver();
+       WebDriver driver = new FirefoxDriver();
 
         driver.navigate().to("https://www.hackerearth.com/");
         driver.manage().window().maximize();
-       WebElement login = driver.findElement(By.cssSelector("#__next > div > div > div.header_2_OMF > div > div.buttons_1nn8B > div:nth-child(1) > span"));
+       WebElement login = driver.findElement(By.cssSelector("#__next > div > div > div.header > div > div.buttons > div:nth-child(1)"));
         login.click();
         WebElement email= driver.findElement(By.name("username"));
-        email.sendKeys("......");
+        email.sendKeys("500068360@stu.upes.ac.in");
         WebElement password= driver.findElement(By.name("password"));
-        password.sendKeys(".......");
-        WebElement log= driver.findElement(By.className("submitButton_uHU80"));
+        password.sendKeys("QV1aID@1");
+        WebElement log= driver.findElement(By.className("submitButton"));
         log.click();
         System.out.println("login Successful");
     }}
